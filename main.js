9dbe93121815aaee571eb4533292e881b3664122
Glauber S <glauber.silva14@gmail.com>
2024-03-13 17:49:50 -0300

    chore: improve information of log messages

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ const { printReport } = require('./report.js');
 
 async function main() {
     const ARGV_EXPECTED_SIZE = 3;
+    if (argv.length < ARGV_EXPECTED_SIZE){
+        console.log('no website provided')
+    }
 
-    if(argv.length !== ARGV_EXPECTED_SIZE ) {
-        throw new Error('You must provide one argument as the baseURL');
-        return;
+    if (argv.length > ARGV_EXPECTED_SIZE){
+        console.log('too many arguments provided')
     }
 
     const baseURL = argv[2];
-    console.log(`Starting crawler: ${baseURL}`);
+    console.log(`Starting crawling of: ${baseURL}`);
 
     const res = await crawlPage(baseURL, baseURL, {});
+
     printReport(res);
 }
 
